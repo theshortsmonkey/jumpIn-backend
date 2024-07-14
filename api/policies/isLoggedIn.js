7781e64313c44c1bed33342809b7d44c75510476
theshortsmonkey <j.horsley72@gmail.com>
2024-07-14 12:34:01 +0100

    udpated prod config

--- api/policies/isLoggedIn.js
+++ api/policies/isLoggedIn.js
@@ -5,7 +5,6 @@ module.exports = async function (req, res, proceed) {
   // or, if this is the last policy, the relevant action.
   // > For more about where `req.me` comes from, check out this app's
   // > custom hook (`api/hooks/custom/index.js`).
-  console.log(req.session)
   if (req.session.userId) {
     return proceed();
   }
